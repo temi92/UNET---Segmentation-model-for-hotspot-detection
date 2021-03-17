846c0f62a5848948b3cf296dfc46ff6379bf2691
temi92 <odusinatemitope14@yahoo.com>
2021-03-17 23:25:53 +0000

    minor fix

--- evaluation.py
+++ evaluation.py
@@ -10,38 +10,32 @@ from pathlib import Path
 from models.mobilenet_unet import unet_model
 from image_processor import load_and_preprocess_testImage
 from utils.visualize_images import plot_orig_predMask
+from datetime import datetime
+import os
 
-model_filename = "weights-improvement-max-60-0.26.hdf5"
+#model_filename = "weights/weights-improvement-max-60-0.26.hdf5"
+#model_filename = "weights/best_model.hdf5"
 
 argparser = argparse.ArgumentParser(description=__doc__)
 argparser.add_argument('-d','--dataset',help='folder containing images',\
         required=True,
         default=None)
+argparser.add_argument('-w','--weights', help='paths to weights file',\
+        required=True,
+        default=None)
+
+argparser.add_argument('-b','--batch_size', type=int, help='batch size',\
+        default=32)
 
 args = argparser.parse_args()
-batch_size=16
-
-
-def display(display_list):
-    import matplotlib.pyplot as plt
-
-    plt.figure(figsize=(15, 15))
-
-    title = ['Input Image', "Predicted Mask"]
-
-    for i in range(2):
-        plt.subplot(1, 2, i+1)
-        plt.title(title[i])
-        plt.imshow(display_list[i])
-        plt.axis('off')
-    plt.show()
-    
 
 def main():
+    batch_size = args.batch_size
+    weights = args.weights
     model = unet_model()
-    model.load_weights(model_filename)
+    model.load_weights(weights)
     images = Path(args.dataset)
-    
+ 
     img_paths = [str(path) for path in list(images.glob('*.jpg'))]
     test_dataset = tf.data.Dataset.from_tensor_slices(img_paths)
     test_dataset = test_dataset.map(load_and_preprocess_testImage).batch(batch_size)
